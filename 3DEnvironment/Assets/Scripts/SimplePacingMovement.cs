f7ad1d250b5b79e9e29542cd6e67eeec238db968
Shen Yang <yangshen@gatech.edu>
2015-07-16 21:20:29 -0400

    Added till 5,6

---
+++ 3DEnvironment/Assets/Scripts/SimplePacingMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePacingMovement : MonoBehaviour {
+
+	public float movementRangeX = 5.0f;
+	public float movementSpeed = 10.0f;
+	Vector3 originalPosition;
+
+	public float currentTimeInSeconds = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		originalPosition = transform.localPosition;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTimeInSeconds += Time.deltaTime;
+		float xMovement = Mathf.PingPong (currentTimeInSeconds, movementRangeX);
+		Vector3 newPosition = originalPosition;
+		newPosition.z += xMovement * movementSpeed;
+		transform.localPosition = newPosition;
+	
+	}
+}
