5a7613be4e37e599948daac9163c42ae0f4a0d5d
whileLooper <bochentheone@gmail.com>
2015-07-23 21:05:37 -0400

    add event control

---
+++ MiniProject/Assets/Script/EventControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventControl : MonoBehaviour {
+
+	Transform trans;
+	bool isStandup = false;
+	// Use this for initialization
+	void Start () {
+		trans = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1")) {
+			if (isStandup) {
+				trans.localScale = new Vector3(1.0f, 1.8f, 1.0f);
+				isStandup = !isStandup;
+			}
+			else {
+				trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+				isStandup = !isStandup;
+			}
+		}
+	}
+
+}
